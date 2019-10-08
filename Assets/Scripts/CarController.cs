222ee3f5b4e4b5ae610185be6e67dbf5748448d7
Tony Ta <manhtuongta1996@gmail.com>
2019-10-09 00:25:19 +1100

    second Commit
    adding car movement

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public static int movespeed = 10;
+    public Vector3 userDirection = Vector3.right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * Time.deltaTime * movespeed;
+    }
+}
