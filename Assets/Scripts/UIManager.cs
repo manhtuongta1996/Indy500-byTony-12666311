5ea8ced136062045a960f45ac128f8e9294f9dde
Tony Ta <manhtuongta1996@gmail.com>
2019-10-13 20:19:43 +1100

    StartScene
    added start scene

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadSceneAsync(0);
+        Object.DontDestroyOnLoad(this.gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex == 0)
+        {
+            Button QuitButton = GameObject.FindGameObjectWithTag("QuitButton").GetComponent<Button>();
+            QuitButton.onClick.AddListener(QuitGame);
+        }
+    }
+}
